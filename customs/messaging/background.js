ccd8807f8f0b6ce6d6888982368f20f9a237d776
Tom PERRILLAT-COLLOMB <perrillatcollomb.tom@gmail.com>
2022-04-12 23:37:18 +0200

    :sparkles: add messaging exemple from content to background

--- customs/messaging/background.js
+++ customs/messaging/background.js
@@ -1,9 +1,22 @@
 // Background listener
 chrome.runtime.onConnect.addListener(function(port) {
     console.log("[BACKGROUND] Port name: " + port.name);
-    console.assert(port.name === "joking");
     port.onMessage.addListener(function(msg) {
-        if (msg.joke === "fart")
-            port.postMessage({answer: "Funny guy :)"});
+        switch (port.name) {
+            case "joking":
+                if (msg.joke === "fart")
+                    port.postMessage({answer: "Funny guy :)"});
+                break;
+            case "cookies":
+                if (msg.question === "number") {
+                    // get all cookies async, send respond when api returns cookies
+                    chrome.cookies.getAll({domain: "google.com"}, (res) => {
+                        console.log("[BACKGROUND] Cookies: " + res.length);
+                        port.postMessage({answer: res.length});
+                    });
+                }
+                break;
+        }
     });
+    
 });
--- customs/messaging/popup.js
+++ customs/messaging/popup.js
@@ -1,11 +1,14 @@
 // query buttons
-let call_background = document.querySelector("#call_background");
-let call_content = document.querySelector("#call_content");
+let call_extension_background = document.querySelector("#call_extension_background");
+let call_extension_content = document.querySelector("#call_extension_content");
+let call_content_background = document.querySelector("#call_content_background");
 let inject_content = document.querySelector("#inject_content");
 
-call_background.addEventListener("click", () => {
+// Calling background and getting response
+call_extension_background.addEventListener("click", () => {
 
     console.log("[EXTENSION] Calling background...");
+
     let port = chrome.runtime.connect({name: "joking"});
     port.postMessage({joke: "fart"});
     port.onMessage.addListener(function(msg) {
@@ -14,10 +17,10 @@ call_background.addEventListener("click", () => {
 
 });
 
-call_content.addEventListener("click", async () => {
+// Calling content and getting response
+call_extension_content.addEventListener("click", async () => {
 
     console.log("[EXTENSION] Calling content...");
-
 
     chrome.runtime.onMessage.addListener(
         function(request, sender, sendResponse) {
@@ -37,7 +40,9 @@ call_content.addEventListener("click", async () => {
 
 });
 
+// Injecting script in content
 inject_content.addEventListener("click", async () => {
+
     console.log("[EXTENSION] Injecting content...");
 
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
@@ -48,15 +53,50 @@ inject_content.addEventListener("click", async () => {
 
 });
 
+// Injecting script in content, that calls background and receive a response
+call_content_background.addEventListener("click", async () => {
+
+    console.log("[EXTENSION] Calling content...");
+
+    chrome.runtime.onMessage.addListener(
+        function(request, sender, sendResponse) {
+            console.log("[EXTENSION] Message received from content script");
+            if (request.nb) {
+                console.log("[EXTENSION] Nb cookies: " + request.nb);
+                sendResponse({});
+            }
+        }
+    );
+
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: askNbCookies,
+    });
+
+});
+
+// script to inject
 const changeBackgroundColor = () => {
     console.log("[CONTENT SCRIPT] Changing background color")
     document.body.style.backgroundColor = "red";
 }
 
+// script to inject to communicate with extension
 const fetchNumberOfDivs = () => {
     console.log("[CONTENT SCRIPT] Fetching number of divs in page")
     let nb_divs = document.querySelectorAll("div").length;
     chrome.runtime.sendMessage({nb: nb_divs}, function(response) {
         console.log("[CONTENT SCRIPT] Sent: " + nb_divs);
+    });
+}
+
+// script to inject to communicate with background
+const askNbCookies = () => {
+    console.log("[CONTENT SCRIPT] Asking background number of cookies")
+    let port = chrome.runtime.connect({name: "cookies"});
+    port.postMessage({question: "number"});
+    port.onMessage.addListener(function(msg) {
+        console.log("[CONTENT SCRIPT] Response: " + msg.answer);
     });
 }
